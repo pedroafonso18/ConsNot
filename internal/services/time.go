16b57f98d3c9981b1403e93399941de07ee9afa5
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-07-18 13:15:54 -0300

    chore: changed working time

--- internal/services/time.go
+++ internal/services/time.go
@@ -4,7 +4,7 @@ import "time"
 
 func IsAllowedTime() bool {
 	currentTime := time.Now()
-	startTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 11, 27, 0, 0, currentTime.Location())
+	startTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 21, 30, 0, 0, currentTime.Location())
 	endTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 2, 30, 0, 0, currentTime.Location())
 
 	if startTime.Before(endTime) || startTime.Equal(endTime) {
