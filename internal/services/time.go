83aebe47ab775e4ae8223b7715c31d4f59d29362
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-07-21 08:30:15 -0300

    feat: Added new weekday validation

--- internal/services/time.go
+++ internal/services/time.go
@@ -4,9 +4,12 @@ import "time"
 
 func IsAllowedTime() bool {
 	currentTime := time.Now()
-	startTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 21, 30, 0, 0, currentTime.Location())
+	startTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 7, 30, 0, 0, currentTime.Location())
 	endTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 2, 30, 0, 0, currentTime.Location())
-
+	weekday := currentTime.Weekday()
+	if weekday == time.Saturday || weekday == time.Sunday {
+		return false
+	}
 	if startTime.Before(endTime) || startTime.Equal(endTime) {
 		return (currentTime.Equal(startTime) || currentTime.After(startTime)) && (currentTime.Equal(endTime) || currentTime.Before(endTime))
 	} else {
